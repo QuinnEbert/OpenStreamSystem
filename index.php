5afd41b9eebb2bccc8603da228f12312c4f07908
Quinn Ebert <quinnebert@gmail.com>
2015-05-24 20:29:08 -0400

    First real steps at index.php

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
+<?php
+$vod = array();
+if ($handle = opendir('/tmp/av')) {
+	while (false !== ($entry = readdir($handle))) {
+        if (strtolower(substr($entry,-4))==='.flv') {
+        	$vod[] = "/tmp/av/{$entry}";
+        }
+    }
+}
+?>
 <html>
 <head>
 <title>OSS</title>
 </head>
 <body>
 <h1>OSS</h1>
-<p>Coming soon...</p>
+<p>Still in <em>heavy</em> development...</p>
+<h2>VOD List (TEST only)</h2>
+<pre><?php
+echo(print_r($vod,true));
+?></pre>
 </body>
 </html>
