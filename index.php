04b8acaca5e6010158bacd958038ac038ea8b9aa
Quinn Ebert <quinnebert@gmail.com>
2015-05-24 20:40:21 -0400

    More POC/test work on index.php script

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
-$vod = array();
-if ($handle = opendir('/tmp/av')) {
-	while (false !== ($entry = readdir($handle))) {
-        if (strtolower(substr($entry,-4))==='.flv') {
-        	$vod[] = "/tmp/av/{$entry}";
-        }
-    }
+function modal_ajax_return_vod_list() {
+	$vod = array();
+	if ($handle = opendir('/tmp/av')) {
+		while (false !== ($entry = readdir($handle))) {
+			if (strtolower(substr($entry,-4))==='.flv') {
+				$vod[] = "/tmp/av/{$entry}";
+			}
+		}
+	}
+	$die = json_encode(array('count'=>count($vod),'data'=>$vod));
+	if (isset($_REQUEST['callback']))
+		die($_REQUEST['callback'].'('.$die.');');
+	die($die);
+}
+if (isset($_GET['modal'])) {
+	switch($_GET['modal']) {
+		case 'vod_list':
+			modal_ajax_return_vod_list();
+			break;
+	}
 }
 ?>
 <html>
@@ -16,8 +29,22 @@ if ($handle = opendir('/tmp/av')) {
 <h1>OSS</h1>
 <p>Still in <em>heavy</em> development...</p>
 <h2>VOD List (TEST only)</h2>
-<pre><?php
-echo(print_r($vod,true));
-?></pre>
+<pre id="vod_list"></pre>
+<script type="text/javascript">
+function modal_ajax_return_vod_list(dataSet) {
+	var elem = document.getElementById('vod_list');
+	elem.innerHTML = '';
+	if (!dataSet.count) {
+		elem.innerHTML += 'No VODs!';
+	} else {
+		for (var i = 0 ; i < dataSet.count ; i = i + 1) {
+			elem.innerHTML += dataSet.data[i]+"\n";
+		}
+	}
+}
+var script = document.createElement('script');
+script.src = 'index.php?modal=vod_list&callback=modal_ajax_return_vod_list'
+document.getElementsByTagName('head')[0].appendChild(script);
+</script>
 </body>
 </html>
